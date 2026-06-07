1078ba613f5e1fc36c45d16b30b9c4075237676c
Aaron Liddell <aaronliddell@gmail.com>
2026-06-07 13:11:36 +0100

    linear layer working, tensor converted to float

--- pytorchPractise.py
+++ pytorchPractise.py
@@ -4,7 +4,7 @@ import torch.nn as nn #neural network
 data = [1, 2, 3, 4, 5]
 extra_data = [1, 3, 2, 3, 6]
 
-tensor = torch.tensor(data)
+tensor = torch.tensor(data, dtype=torch.float32)
 error = torch.tensor(extra_data)
 
 print(tensor)
@@ -15,8 +15,8 @@ tensor_diff = tensor + error #addition
 print(tensor_diff)
 tensor_element_mult = tensor * error #element multiplied
 print(tensor_element_mult)
-tensor_matrix_mult = tensor @ error #matrix multiplied 
-print(tensor_matrix_mult)
+#tensor_matrix_mult = tensor @ error #matrix multiplied (doesnt work with float data type)
+#print(tensor_matrix_mult)
 
 linear_layer = nn.Linear(5, 2, 2)
 result = linear_layer(tensor)
