ee3f0ae058ac9e75a12a3ab16ebead466e4b6c9d
Aaron Liddell <aaronliddell@gmail.com>
2026-06-07 13:14:21 +0100

    commented

--- pytorchPractise.py
+++ pytorchPractise.py
@@ -18,7 +18,11 @@ print(tensor_element_mult)
 #tensor_matrix_mult = tensor @ error #matrix multiplied (doesnt work with float data type)
 #print(tensor_matrix_mult)
 
-linear_layer = nn.Linear(5, 2, 2)
+linear_layer = nn.Linear(5, 2, 1) 
+#(number of inputs, number of outputs, True(1)/False(0) to include bias)
+#currently weight matrix is random so output in meaningless
+#input (5 numbers) x weight matrix (5x2) + bias (2 numbers) = output (2 numbers)
+
 result = linear_layer(tensor)
 print(f"Result: {result}")
 
