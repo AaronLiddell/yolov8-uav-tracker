5e15a404d14f09e3b72d5cf23cc38d14646937fd
Aaron Liddell <aaronliddell@gmail.com>
2026-06-30 12:42:15 +0100

    add CIFAR to project repo

---
+++ CIFAR10/mainCIFAR.py
@@ -0,0 +1,7 @@
+#CIFAR10
+#‘airplane’, ‘automobile’, ‘bird’, ‘cat’, ‘deer’, ‘dog’, ‘frog’, ‘horse’, ‘ship’, ‘truck’
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
