f7ff6f6a93b297d1de85589d20cce32e0702b156
Aaron Liddell <aaronliddell@gmail.com>
2026-08-03 21:09:44 +0100

    changed from built in ultralytics tracking and boxing to openCV based

--- PractiseExamples/YOLOv8/yoloTest.py
+++ PractiseExamples/YOLOv8/yoloTest.py
@@ -21,14 +21,28 @@ def CaptureVideo():
             print("Cant recieve frame (stream end?). Exiting....")
             break
 
-        result = model(frame, save=False)
-        annotated = result[0].plot()
-        cv.imshow("frame", annotated)
+        results = model(frame, save=False)
+        boxes = results[0].boxes
+
+        for box in boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0]) #gives top left and bottom right corners of bounding boxes and turns from tensor to integer
+            conf = float(box.conf[0]) #pulls confidence score of a box. converts to float
+            cls = int(box.cls[0]) #pulls out class number and converts to interger
+            label = model.names[cls] #gets the label from the class number
+
+            vertRectangle(frame,x1,y1,x2,y2)
+
+            cv.putText(frame, f"{label} {conf:.2f}", (x1,y1), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
+
+        cv.imshow("frame", frame)
 
         if cv.waitKey(1) == ord("q"): #waits for q pressed to end 
             break
 
     cap.release()
     cv.destroyAllWindows()
+
+def vertRectangle(frame,x1,y1,x2,y2):
+    cv.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 2)
 
 CaptureVideo()
